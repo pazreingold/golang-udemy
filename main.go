afb8204e2b2e4defa5d86d9e6e29c30f29b5a17e
pazrein <reingoldpaz@gmail.com>
2023-08-11 19:42:20 +0300

    Add Basic main Function

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	cards := []string{newCard(), newCard()}
+	cards = append(cards, newCard())
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
+}
