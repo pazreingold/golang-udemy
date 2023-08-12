637233190889421b6d4dc3da7b52b2f827184ce4
pazrein <reingoldpaz@gmail.com>
2023-08-12 11:58:47 +0300

    Implement newDeck

--- deck.go
+++ deck.go
@@ -4,6 +4,22 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardsSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardsSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
--- main.go
+++ main.go
@@ -1,13 +1,6 @@
 package main
 
 func main() {
-	cards := deck{newCard(), newCard()}
-	cards = append(cards, newCard())
-
+	cards := newDeck()
 	cards.print()
-
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
