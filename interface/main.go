eb942d29e3337c545d01ae2754999ebca343a8ef
Pazreingold <pazreingold@catonetworks.com>
2023-08-14 10:52:25 +0300

    Use Interface

--- interface/main.go
+++ interface/main.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+type bot interface {
+	getGreeting() string
+}
 type englishBot struct {
 }
 
@@ -24,10 +27,6 @@ func (spanishBot) getGreeting() string {
 	return "Hola!"
 }
 
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
